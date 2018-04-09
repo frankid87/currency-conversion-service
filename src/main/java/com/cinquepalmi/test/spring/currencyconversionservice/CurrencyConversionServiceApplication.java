0a398222327387207695bec734240fc4cd426a61
frankid <f_cinquepalmi@yahoo.it>
2018-04-10 00:16:38 +0200

    Add eureka

--- src/main/java/com/cinquepalmi/test/spring/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ src/main/java/com/cinquepalmi/test/spring/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -2,10 +2,12 @@ package com.cinquepalmi.test.spring.currencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients("com.cinquepalmi.test.spring.currencyconversionservice.feign")
+@EnableDiscoveryClient
 public class CurrencyConversionServiceApplication {
 
 	public static void main(String[] args) {
