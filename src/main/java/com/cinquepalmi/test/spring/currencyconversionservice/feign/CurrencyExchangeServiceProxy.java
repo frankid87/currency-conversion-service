de3447f9366a4e1b75b890f599457b9158ff7702
Francesco Cinquepalmi <frcinquepa@OWGROUP.IT>
2018-04-15 23:57:21 +0200

    Consegna pc

--- src/main/java/com/cinquepalmi/test/spring/currencyconversionservice/feign/CurrencyExchangeServiceProxy.java
+++ src/main/java/com/cinquepalmi/test/spring/currencyconversionservice/feign/CurrencyExchangeServiceProxy.java
@@ -26,17 +26,25 @@ import com.cinquepalmi.test.spring.currencyconversionservice.dto.ConversionCurre
 /*
  * Ribbon, Feign e Eureka
  * 
- * l'attributo name deve corrispondere al nome con la quale l'applicazione è registrtata su Eureka
+ * l'attributo name deve corrispondere al nome con il quale l'applicazione è registrata su Eureka
  */
 //@FeignClient(name="CURRENCY-EXCHANGE-SERVICE") 
 //@RibbonClient(name="CURRENCY-EXCHANGE-SERVICE")
 
+/*
+ * Ribbon, Feign, Eureka e Zuul
+ * 
+ * l'attributo name corrisponde al nome con il quale zuul-apigateway-server è registrato su Eureka
+ */
+//@FeignClient(name="NETFLIX-ZUUL-API-GATEWAY-SERVER") 
+//@RibbonClient(name="CURRENCY-EXCHANGE-SERVICE")
 
-@FeignClient(name="CURRENCY-EXCHANGE-SERVICE") 
+@FeignClient(name="NETFLIX-ZUUL-API-GATEWAY-SERVER") 
 @RibbonClient(name="CURRENCY-EXCHANGE-SERVICE")
 public interface CurrencyExchangeServiceProxy {
 
-	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+//	@GetMapping("/currency-exchange/from/{from}/to/{to}") commentato con introduzione zuul 
+	@GetMapping("/currency-exchange-service/currency-exchange/from/{from}/to/{to}") // con zuul il path diviene http://host:port/{microservice name}/{path}
 	public ConversionCurrencyBean retrieveExchangeValue(@PathVariable("from") String from, @PathVariable("to") String to);
 	
 }
